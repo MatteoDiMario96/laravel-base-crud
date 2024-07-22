701a14d8864fa70453dc258cc48096acae951c17
MatteoDiMario96 <dm.matteo96@live.it>
2024-07-22 16:45:45 +0200

    Una home praticamnete vuota solo collegamento all'index.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,15 @@
 @section('title-page', 'Template Creato!')
 
 @section('main-content')
-    <h1>
-        Template Creato
-    </h1>
+    <header class="m-5">
+        <h1>
+            Homepage
+        </h1>
+    </header>
+    <main class="m-5">
+        <h2 class="mb-5">
+            Vieni a vedere la lista degli animali !
+        </h2>
+        <a href="{{route('pages.animals.index')}}" class="btn btn-info">Vedi la lista completa</a>
+    </main>
 @endsection
