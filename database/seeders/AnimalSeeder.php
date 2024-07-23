6d1c642c14dd2a8572ecdda5c5a70297ac8d8602
MatteoDiMario96 <dm.matteo96@live.it>
2024-07-23 17:16:59 +0200

    Pushato con .csv

--- database/seeders/AnimalSeeder.php
+++ database/seeders/AnimalSeeder.php
@@ -15,34 +15,34 @@ class AnimalSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        // $animalsData = Helpers::getCsvInfo(__DIR__ . '/../../resources/assets/animal_data.csv');
-        // foreach ($animalsData as $index => $animal) {
-        //     if ($index > 0) {
-        //         $newAnimal = new Animals();
-        //         $newAnimal->common_name = $animal[1];
-        //         $newAnimal->scientific_name = $animal[2];
-        //         $newAnimal->class = $animal[3];
-        //         $newAnimal->habitat = $animal[4];
-        //         $newAnimal->geographical_distribution = $animal[5];
-        //         $newAnimal->high_max = $animal[6];
-        //         $newAnimal->weigh_max = $animal[7];
-        //         $newAnimal->note = $animal[8];
-        //         $newAnimal->save();
-        //     }
-        // }
-
-        for ($i=0; $i < 50; $i++) {
-            $newAnimal = new Animals();
-            $newAnimal->common_name = $faker->name();
-            $newAnimal->scientific_name = $faker->firstName();
-            $newAnimal->class = $faker->word();
-            $newAnimal->habitat = implode(' ', $faker->words(3));
-            $newAnimal->geographical_distribution = implode(' ', $faker->words(4));
-            $newAnimal->high_max = $faker->numberBetween(0, 200);
-            $newAnimal->weigh_max = $faker->numberBetween(0, 150);
-            $newAnimal->note = implode(' ', $faker->words(10));
-            $newAnimal->save();
+        $animalsData = Helpers::getCsvInfo(__DIR__ . '/../../resources/assets/animal_data.csv');
+        foreach ($animalsData as $index => $animal) {
+            if ($index > 0) {
+                $newAnimal = new Animals();
+                $newAnimal->common_name = $animal[1];
+                $newAnimal->scientific_name = $animal[2];
+                $newAnimal->class = $animal[3];
+                $newAnimal->habitat = $animal[4];
+                $newAnimal->geographical_distribution = $animal[5];
+                $newAnimal->high_max = $animal[6];
+                $newAnimal->weigh_max = $animal[7];
+                $newAnimal->note = $animal[8];
+                $newAnimal->save();
+            }
         }
+
+        // for ($i=0; $i < 50; $i++) {
+        //     $newAnimal = new Animals();
+        //     $newAnimal->common_name = $faker->name();
+        //     $newAnimal->scientific_name = $faker->firstName();
+        //     $newAnimal->class = $faker->word();
+        //     $newAnimal->habitat = implode(' ', $faker->words(3));
+        //     $newAnimal->geographical_distribution = implode(' ', $faker->words(4));
+        //     $newAnimal->high_max = $faker->numberBetween(0, 200);
+        //     $newAnimal->weigh_max = $faker->numberBetween(0, 150);
+        //     $newAnimal->note = implode(' ', $faker->words(10));
+        //     $newAnimal->save();
+        // }
     }
 }
 
