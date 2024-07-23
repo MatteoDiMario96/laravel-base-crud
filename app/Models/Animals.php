edc7ac9c35d4381bf29ffaa70c870c98f932081b
MatteoDiMario96 <dm.matteo96@live.it>
2024-07-23 22:38:05 +0200

    Creato fillable per seedare piu velocemente.

--- app/Models/Animals.php
+++ app/Models/Animals.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Animals extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'common_name',
+        'scientific_name',
+        'class',
+        'habitat',
+        'geographical_distribution',
+        'high_max',
+        'weigh_max',
+        'image_url',
+        'note',
+    ];
 }
