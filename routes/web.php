3cfb57b0519174ffd30f84aa785ecf451a1e3ab1
MatteoDiMario96 <dm.matteo96@live.it>
2024-07-25 16:03:19 +0200

    Create le rotteget e put per editare e delete per eliminare .

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'view']) ->name('pages.home');
-
 Route::get('/animals', [AnimalController::class, 'index']) ->name('pages.animals.index');
-
 Route::post('/animals', [AnimalController::class, 'store']) ->name('animals.store');
-
-
 Route::get('/animals/create', [AnimalController::class, 'create']) ->name('pages.animals.create');
-
 Route::get('/animals/{animal}', [AnimalController::class, 'show']) ->name('pages.animals.show');
+Route::put('/animals/{animal}', [AnimalController::class, 'update']) ->name('animals.update');
+Route::delete('/animals/{animal}', [AnimalController::class, 'destroy']) ->name('animals.destroy');
+Route::get('/animals/{animal}/edit', [AnimalController::class, 'edit']) ->name('pages.animals.edit');
+
 
 
 
