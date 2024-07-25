8e2ad4989dbde96f73bbf8a3a5dbb00df6b2074c
MatteoDiMario96 <dm.matteo96@live.it>
2024-07-25 18:35:03 +0200

    Creata una variabile che mi va a prendere l'attributo nel tag HTML data-animal-name per poi assegnarlo dinamicamente nell if dell window.confirm.

--- resources/js/delete.js
+++ resources/js/delete.js
@@ -3,10 +3,10 @@ const formsDelete = document.querySelectorAll('form.forms-destroy');
 formsDelete.forEach(deleteFormEl => {
     deleteFormEl.addEventListener('submit', function (event){
         event.preventDefault();
+        const animalName = this.getAttribute('data-animal-name');
 
-        if(window.confirm('Do you want really DELETE that animal ?') === true){
+        if(window.confirm(`Do you want MOVE in the Trash that ${animalName}, are you sure ?`) === true){
             this.submit();
         }
     })
 });
-console.log('ciao');
--- resources/js/edit.js
+++ resources/js/edit.js
@@ -2,10 +2,11 @@ const formEdit = document.querySelector('form.form-edit');
 
 formEdit.addEventListener('submit', function (event){
     event.preventDefault();
+    const animalName = this.getAttribute('data-animal-name');
 
 
 
-    if(window.confirm('Do you want EDIT that animal with that generality ?') === true){
+    if(window.confirm(`Do you want EDIT that ${animalName} with that generality ?`) === true){
         this.submit();
     }
 })
