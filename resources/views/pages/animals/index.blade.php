0647053acd81a542f56f8f30f9bb90f25d70073d
MatteoDiMario96 <dm.matteo96@live.it>
2024-07-25 15:59:18 +0200

    Creati i tre botti edita elimina e e vedi dettagli. Con le relative rotte.

--- resources/views/pages/animals/index.blade.php
+++ resources/views/pages/animals/index.blade.php
@@ -2,6 +2,10 @@
 
 @section('title-page', 'Lista Animali')
 
+@section('script-js')
+    @vite('resources/js/delete.js')
+@endsection
+
 @section('main-content')
 <h1 class="text-center">
     Lista Animali
@@ -14,7 +18,7 @@
             <th scope="col">Scientific Name</th>
             <th scope="col">Class</th>
             <th scope="col">Habitat</th>
-            <th scope="col">Info</th>
+            <th scope="col" class="text-center">Info</th>
 
         </tr>
     </thead>
@@ -26,12 +30,22 @@
         <td>{{$singleAnimal->scientific_name}}</td>
         <td>{{$singleAnimal->class}}</td>
         <td>{{$singleAnimal->habitat}}</td>
-        <td><a href="{{route('pages.animals.show', $singleAnimal->id)}}" class="btn btn-success btn-sm">Dettagli</a></td>
+        <td class="text-center">
+            <a href="{{route('pages.animals.show', $singleAnimal)}}" class="btn btn-info btn-sm m-2">Details</a>
+            <a href="{{route('pages.animals.edit', $singleAnimal)}}" class="btn btn-success btn-sm m-2">Edit</a>
+            <form action="{{route('animals.destroy', $singleAnimal)}}" method="POST" class="d-inline-block forms-destroy">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-warning btn-sm m-2">
+                    Delete
+                </button>
+            </form>
+        </td>
     </tr>
     @endforeach
 </table>
 
 <div class="row my-3">
-    <a href="{{route('pages.animals.create')}}" class="btn btn-info btn-xl col-12">Aggiungi alla lista un nuovo animale</a>
+    <a href="{{route('pages.animals.create')}}" class="btn btn-danger btn-xl col-12">Aggiungi alla lista un nuovo animale</a>
 </div>
 @endsection
