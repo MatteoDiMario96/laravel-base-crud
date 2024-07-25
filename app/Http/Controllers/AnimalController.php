4375d14541f4a08ed0e4bc2c090ccabff7427325
MatteoDiMario96 <dm.matteo96@live.it>
2024-07-25 20:45:14 +0200

    Cambiate le Request con le request di validation. Infatti ho anche cambiaot nei metodi a loro associati cioè store e update da $request->all() a validated()

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -2,72 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreAnimalRequest;
+use App\Http\Requests\UpdateAnimalRequest;
 use App\Models\Animals;
-use Illuminate\Http\Request;
 
 class AnimalController extends Controller
 {
     public function index(){
         $animals = Animals::all();
-
         return view('pages.animals.index', compact('animals'));
     }
 
     public function show(Animals $animal){
-
         return view('pages.animals.show', compact('animal'));
-
     }
 
     public function create(){;
-
         return view('pages.animals.create');
-
     }
 
-    public function store(Request $request){;
-
-        $data = $request->all();
-
+    public function store(StoreAnimalRequest $request){;
+        $data = $request->validated();
         $newAnimal = new Animals($data);
         $newAnimal->save();
         return redirect(route('pages.animals.show', $newAnimal));
-
-
-
     }
+
     public function edit(Animals $animal){;
-
-
         return view('pages.animals.edit', compact('animal'));
-
-
-
     }
-    public function update(Request $request, Animals $animal ){;
 
-        $data = $request->all();
-
+    public function update(UpdateAnimalRequest $request, Animals $animal ){;
+        $data = $request->validated();
         $animal->update($data);
         return redirect(route('pages.animals.show', $animal))->with('edit-animal', $animal->common_name . ' '. 'has been edited with success');
-
-
     }
+
     public function destroy( Animals $animal ){;
         $animal->delete();
         return redirect()->route('pages.animals.index')->with('deleted-message', $animal->common_name . ' '. 'has been moved on the trash backet');
-
     }
 
     public function deletedIndex(){
         $animals = Animals::onlyTrashed()->get();
         return view('pages.animals.trash-index', compact('animals'));
     }
+
     public function restore(string $id){
         $animal = Animals::onlyTrashed()->findOrFail($id);
         $animal->restore();
         return redirect()->route('pages.animals.trash-index')->with('deleted-message', $animal->common_name . ' '. 'has been restored');
     }
+
     public function permanentDelete(string $id){
         $animal = Animals::onlyTrashed()->findOrFail($id);
         $animal->forceDelete();
