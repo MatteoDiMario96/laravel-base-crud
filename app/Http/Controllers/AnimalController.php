261c8b6f02b1c216924880b7322cb82fc0cf8976
MatteoDiMario96 <dm.matteo96@live.it>
2024-07-25 16:06:22 +0200

    Aggiunti metodi update edit e destroy. EDIT ritorna la view della pagina edit. UPDATE prende la richiesta dell'intero form lo aggiorna con il metodo update e ridireziona alla pagina .show DESTROY cancella l'animale e rindirizza all'index.

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -13,8 +13,7 @@ class AnimalController extends Controller
         return view('pages.animals.index', compact('animals'));
     }
 
-    public function show($id){
-        $animal = Animals::findOrFail($id);
+    public function show(Animals $animal){
 
         return view('pages.animals.show', compact('animal'));
 
@@ -34,6 +33,29 @@ class AnimalController extends Controller
         $newAnimal->save();
         return redirect(route('pages.animals.show', $newAnimal));
 
+
+
+    }
+    public function edit(Animals $animal){;
+
+
+        return view('pages.animals.edit', compact('animal'));
+
+
+
+    }
+    public function update(Request $request, Animals $animal ){;
+
+        $data = $request->all();
+
+        $animal->update($data);
+        return redirect(route('pages.animals.show', $animal));
+
+
+    }
+    public function destroy( Animals $animal ){;
+        $animal->delete();
+        return redirect(route('pages.animals.index'));
 
 
     }
