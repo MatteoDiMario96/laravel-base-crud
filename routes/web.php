a3d4d017ad5d15fa6ca06c5b20c2f4d5cd961487
MatteoDiMario96 <dm.matteo96@live.it>
2024-07-25 18:45:13 +0200

    Aggiunte le nuove rotte.

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,16 @@ Route::get('/', [PageController::class, 'view']) ->name('pages.home');
 Route::get('/animals', [AnimalController::class, 'index']) ->name('pages.animals.index');
 Route::post('/animals', [AnimalController::class, 'store']) ->name('animals.store');
 Route::get('/animals/create', [AnimalController::class, 'create']) ->name('pages.animals.create');
+Route::get('/animals/trash', [AnimalController::class, 'deletedIndex']) ->name('pages.animals.trash-index');
 Route::get('/animals/{animal}', [AnimalController::class, 'show']) ->name('pages.animals.show');
 Route::put('/animals/{animal}', [AnimalController::class, 'update']) ->name('animals.update');
 Route::delete('/animals/{animal}', [AnimalController::class, 'destroy']) ->name('animals.destroy');
+Route::patch('/animals/{animal}/restore', [AnimalController::class, 'restore']) ->name('animals.restore');
 Route::get('/animals/{animal}/edit', [AnimalController::class, 'edit']) ->name('pages.animals.edit');
+Route::delete('/animals/{animal}/permanent-delete', [AnimalController::class, 'permanentDelete']) ->name('animals.permanent-delete');
+
+
+
 
 
 
